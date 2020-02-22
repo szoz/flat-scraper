c68c128e2993b42c71b7c69b2833ab3dc1c14aab
Paweł Tymosiak <szozr2@gmail.com>
2020-02-22 10:53:10 +0100

    Improved html templates Improved template structure and layout

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from database import read_all_flats, read_flat
 app = Flask(__name__)
 
 
+@app.template_filter()
+def format_price(price):
+    """Returns formatted price value."""
+    return f'{float(price):,.0f} zł'.replace(',', ' ')
+
 @app.route('/')
 def index():
     """Redirects to first page of all flat list."""
@@ -14,14 +19,14 @@ def index():
 @app.route('/<int:page_num>')
 def show_all_flats(page_num):
     """Shows all flat records - 10 per page."""
-    return render_template('index.html',
+    return render_template('all_flors.html',
                            offers=read_all_flats(10, page_num))
 
 
 @app.route('/id/<int:flat_id>')
 def show_flat(flat_id):
     """Shows one flat record with given flat_id."""
-    return render_template('index.html',
+    return render_template('one_flor.html',
                            offers=read_flat(flat_id))
 
 
