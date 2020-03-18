13dd7756a2a6a9f318af8428320894b4ba9ca8f4
Paweł Tymosiak <szozr2@gmail.com>
2020-03-18 20:53:42 +0100

    Flask app refactor Small bug fix and code refactor.

--- app.py
+++ app.py
@@ -16,19 +16,18 @@ def format_price(price):
 @app.route('/')
 def index():
     """Redirects to first page of all flat list."""
-    return redirect('/1')
+    return redirect('/ot/1')
 
 
-@app.route('/<int:page_num>', methods=['GET', 'POST'])
+@app.route('/ot/<int:page_num>')
 def show_all_flats(page_num, flats_per_page=20):
     """Shows all flat records with pagination"""
     last_page = get_flat_count() // flats_per_page + 1
     if page_num < 1 or page_num > last_page:
         return abort(404)  # TODO fix pycharm warnings - read about flask support
-    pages = {'current': page_num, 'last': last_page}  # TODO reformat functions
-    return render_template('all_flors.html',
+    pages = {'previous': page_num-1, 'current': page_num, 'next': page_num+1,'last': last_page}
+    return render_template('all_flats.html',
                            offers=read_all_flats(flats_per_page, page_num, request.args.get('sort')), pages=pages)
-# TODO fix error page when flat id is incorrect
 
 
 @app.route('/gt/<int:page_num>')
@@ -37,35 +36,52 @@ def show_all_flats_gt(page_num, flats_per_page=20):
     last_page = get_flat_count(oto=False) // flats_per_page + 1
     if page_num < 1 or page_num > last_page:
         return abort(404)
-    pages = {'current': page_num, 'last': last_page}
-    return render_template('all_flors_gt.html',
+    pages = {'previous': page_num-1, 'current': page_num, 'next': page_num+1,'last': last_page}
+    return render_template('all_flats_gt.html',
                            offers=read_all_flats(flats_per_page, page_num, request.args.get('sort'), oto=False), pages=pages)
 
 
-@app.route('/id/<int:flat_id>')
-def show_flat(flat_id):
-    """Shows one flat record with given flat_id."""
+@app.route('/ot/id/<int:flat_id>')
+def show_flat_ot(flat_id):
+    """Shows one otodom flat record with given flat_id."""
     if read_flat(flat_id)[0] is not None:
         record = read_flat(flat_id)
         timestamped = zip(*(record[0][ts] for ts in ('scraped_date', 'price', 'price_pm', 'added_date', 'updated_date')))
-        return render_template('one_flor.html', offers=record, pages=None, timestamped=timestamped)
-    elif read_flat(flat_id, oto=False)[0] is not None:
+        return render_template('one_flat.html', offers=record, pages=None, timestamped=timestamped)
+    else:
+        return abort(404)
+
+
+@app.route('/gt/id/<int:flat_id>')
+def show_flat_gt(flat_id):
+    """Shows one gumtree flat record with given flat_id."""
+    if read_flat(flat_id, oto=False)[0] is not None:
         record = read_flat(flat_id, oto=False)
-        return render_template('all_flors_gt.html', offers=record, pages=None)
+        return render_template('all_flats_gt.html', offers=record, pages=None)
     else:
         return abort(404)
 
 
 @app.route('/id', methods=['POST'])
 def show_flat_post():
-    """TODO write a docstring"""
-    return redirect(f'/id/{request.form["id"]}')
+    """Shows one flat record based on flat_id sent in form."""
+    # return redirect(f'/id/{request.form["id"]}')
+    flat_id = int(request.form["id"])
+    if read_flat(flat_id)[0] is not None:
+        return show_flat_ot(flat_id)
+    elif read_flat(flat_id, oto=False)[0] is not None:
+        return show_flat_gt(flat_id)
+    else:
+        return abort(404)
 
 
 @app.route('/delete/<int:flat_id>')
 def remove_flat(flat_id):
     """Deletes one flat record with given flat_id."""
-    delete_flat(flat_id)
+    if read_flat(flat_id)[0] is not None:
+        delete_flat(flat_id)
+    else:
+        delete_flat(flat_id, oto=False)
     return '', 204
 
 
